6a51931359f01910ed642c9961557d5e46fbaa48
Enrico Spinielli <enrico.spinielli@gmail.com>
2019-02-04 18:03:00 +0100

    cleanup

--- R/n_E_and_wa2R_EL.R
+++ R/n_E_and_wa2R_EL.R
@@ -12,19 +12,19 @@
 #' @export
 #'
 #' @examples
-#' \dontrun {
+#' \dontrun{
 #' # Calculates the rotation matrix (direction cosine matrix) R_EL
 #' # using n-vector (n_E) and the wander azimuth angle.
 #' R_EL <-  n_E_and_wa2R_EL(n_E, wander_azimuth)
 #' }
 #'
 n_E_and_wa2R_EL <- function(n_E, wander_azimuth) {
-  lat_lon <- n_E2lat_lon(n_E);
+  lat_lon <- n_E2lat_lon(n_E)
 
   # Longitude, -latitude, and wander azimuth are the x-y-z Euler angles (about
   # new axes) for R_EL. See also the second paragraph of Section 5.2 in Gade (2010):
 
   # R_Ee() selects correct E-axes, see R_Ee.R for details
-  R_EL <-   base::t(R_Ee()) %*% xyz2R(lat_lon[2], -lat_lon[1], wander_azimuth)
+  R_EL <-  base::t(R_Ee()) %*% xyz2R(lat_lon[2], -lat_lon[1], wander_azimuth)
   R_EL
 }
