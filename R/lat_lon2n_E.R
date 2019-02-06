e24ffcd6bda5b709e43a6ecd5664df2d0431138f
Enrico Spinielli <enrico.spinielli@eurocontrol.int>
2019-02-06 15:16:45 +0100

    updated docs

--- R/lat_lon2n_E.R
+++ R/lat_lon2n_E.R
@@ -1,6 +1,6 @@
-#' Convert latitude and longitude to n-vector.
+#' Convert (geodetic) latitude and longitude to n-vector.
 #'
-#' @param latitude Geodetic latitude (rad)
+#' @param latitude  Geodetic latitude (rad)
 #' @param longitude Geodetic longitude (rad)
 #'
 #' @return n-vector decomposed in E (3x1 vector) (no unit)
@@ -8,8 +8,11 @@
 #'
 #' @examples
 #' \dontrun{
+#' latitude <- rad(1)
+#' longitude <- rad(2)
 #' n_E <- lat_lon2n_E(latitude, longitude)
 #' }
+#' @seealso \code{\link{n_E2lat_lon}}.
 lat_lon2n_E <- function(latitude, longitude) {
   (base::t(R_Ee()) %*% c(
      sin(latitude),
